6c789f5f20075605ccd3b9723ade28cd8c133a5a
Logon Aluno <logonaluno@local.com>
2019-08-17 11:02:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fernandavedovello/calculadouraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fernandavedovello.calculadouraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fernandavedovello.calculadouraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
